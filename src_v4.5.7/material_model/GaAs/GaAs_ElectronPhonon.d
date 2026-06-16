c0a9e72a7a41e3183eff0641fbf5fa2d52d9ef77
Andrew Grieder <acgrieder@wisc.edu>
2026-06-16 12:43:33 -0500

    added comm material and scattering back

---
+++ src_v4.5.7/material_model/GaAs/GaAs_ElectronPhonon.d
@@ -0,0 +1,86 @@
+src_v4.5.7/material_model/GaAs/GaAs_ElectronPhonon.o: \
+ src_v4.5.7/material_model/GaAs/GaAs_ElectronPhonon.cpp \
+ src_v4.5.7/material_model/GaAs/GaAs_ElectronPhonon.h \
+ src_v4.5.7/material_model/GaAs/GaAs_lattice.h src_v4.5.7/lattice.h \
+ src_v4.5.7/common_headers.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_cblas.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_integration.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_math.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_sys.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_inline.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_machine.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_precision.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_types.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_nan.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_pow_int.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_minmax.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_errno.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_roots.h \
+ src_v4.5.7/comm/ODE.h src_v4.5.7/comm/scalar.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_odeiv2.h \
+ src_v4.5.7/comm/mymp.h \
+ /software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi.h \
+ /software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi_portable_platform.h \
+ src_v4.5.7/comm/matrix3.h src_v4.5.7/comm/vector3.h \
+ src_v4.5.7/comm/Units.h src_v4.5.7/comm/myio.h \
+ src_v4.5.7/comm/constants.h src_v4.5.7/comm/myarray.h \
+ src_v4.5.7/comm/Random.h src_v4.5.7/comm/mymatrix.h \
+ src_v4.5.7/comm/sparse_matrix.h src_v4.5.7/comm/sparse2D.h \
+ src_v4.5.7/comm/Histogram.h src_v4.5.7/comm/GaussianSmapling.h \
+ src_v4.5.7/comm/kmap.h src_v4.5.7/comm/vector3.h src_v4.5.7/parameters.h \
+ src_v4.5.7/material_model/GaAs/GaAs_electron.h src_v4.5.7/electron.h \
+ src_v4.5.7/PumpProbe.h src_v4.5.7/Scatt_Param.h \
+ src_v4.5.7/material_model/GaAs/GaAs_phonon.h src_v4.5.7/phonon.h \
+ src_v4.5.7/ElectronPhonon.h src_v4.5.7/ElectronImpurity.h \
+ src_v4.5.7/scattering_model/ElecImp_Model.h \
+ src_v4.5.7/scattering_model/Coulomb_Model.h \
+ src_v4.5.7/scattering_model/ElecElec_Model.h
+src_v4.5.7/material_model/GaAs/GaAs_ElectronPhonon.h:
+src_v4.5.7/material_model/GaAs/GaAs_lattice.h:
+src_v4.5.7/lattice.h:
+src_v4.5.7/common_headers.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_cblas.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_integration.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_math.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_sys.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_inline.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_machine.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_precision.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_types.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_nan.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_pow_int.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_minmax.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_errno.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_roots.h:
+src_v4.5.7/comm/ODE.h:
+src_v4.5.7/comm/scalar.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_odeiv2.h:
+src_v4.5.7/comm/mymp.h:
+/software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi.h:
+/software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi_portable_platform.h:
+src_v4.5.7/comm/matrix3.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/comm/Units.h:
+src_v4.5.7/comm/myio.h:
+src_v4.5.7/comm/constants.h:
+src_v4.5.7/comm/myarray.h:
+src_v4.5.7/comm/Random.h:
+src_v4.5.7/comm/mymatrix.h:
+src_v4.5.7/comm/sparse_matrix.h:
+src_v4.5.7/comm/sparse2D.h:
+src_v4.5.7/comm/Histogram.h:
+src_v4.5.7/comm/GaussianSmapling.h:
+src_v4.5.7/comm/kmap.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/parameters.h:
+src_v4.5.7/material_model/GaAs/GaAs_electron.h:
+src_v4.5.7/electron.h:
+src_v4.5.7/PumpProbe.h:
+src_v4.5.7/Scatt_Param.h:
+src_v4.5.7/material_model/GaAs/GaAs_phonon.h:
+src_v4.5.7/phonon.h:
+src_v4.5.7/ElectronPhonon.h:
+src_v4.5.7/ElectronImpurity.h:
+src_v4.5.7/scattering_model/ElecImp_Model.h:
+src_v4.5.7/scattering_model/Coulomb_Model.h:
+src_v4.5.7/scattering_model/ElecElec_Model.h:
---
+++ src_v4.5.7/material_model/GaAs/GaAs_electron.d
@@ -0,0 +1,72 @@
+src_v4.5.7/material_model/GaAs/GaAs_electron.o: \
+ src_v4.5.7/material_model/GaAs/GaAs_electron.cpp \
+ src_v4.5.7/material_model/GaAs/GaAs_electron.h src_v4.5.7/comm/mymp.h \
+ src_v4.5.7/comm/scalar.h \
+ /software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi.h \
+ /software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi_portable_platform.h \
+ src_v4.5.7/material_model/GaAs/GaAs_lattice.h src_v4.5.7/lattice.h \
+ src_v4.5.7/common_headers.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_cblas.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_integration.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_math.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_sys.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_inline.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_machine.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_precision.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_types.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_nan.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_pow_int.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_minmax.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_errno.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_roots.h \
+ src_v4.5.7/comm/ODE.h \
+ /home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_odeiv2.h \
+ src_v4.5.7/comm/matrix3.h src_v4.5.7/comm/vector3.h \
+ src_v4.5.7/comm/Units.h src_v4.5.7/comm/myio.h \
+ src_v4.5.7/comm/constants.h src_v4.5.7/comm/myarray.h \
+ src_v4.5.7/comm/Random.h src_v4.5.7/comm/mymatrix.h \
+ src_v4.5.7/comm/sparse_matrix.h src_v4.5.7/comm/sparse2D.h \
+ src_v4.5.7/comm/Histogram.h src_v4.5.7/comm/GaussianSmapling.h \
+ src_v4.5.7/comm/kmap.h src_v4.5.7/comm/vector3.h src_v4.5.7/parameters.h \
+ src_v4.5.7/electron.h src_v4.5.7/PumpProbe.h src_v4.5.7/Scatt_Param.h
+src_v4.5.7/material_model/GaAs/GaAs_electron.h:
+src_v4.5.7/comm/mymp.h:
+src_v4.5.7/comm/scalar.h:
+/software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi.h:
+/software/chtc/el9/spack_v22/software/linux-centos9-zen3/gcc-13.2.0/openmpi-5.0.3-afcqsnihyo5jvnhepiwqianpo5nwwdci/include/mpi_portable_platform.h:
+src_v4.5.7/material_model/GaAs/GaAs_lattice.h:
+src_v4.5.7/lattice.h:
+src_v4.5.7/common_headers.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_cblas.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_integration.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_math.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_sys.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_inline.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_machine.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_precision.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_types.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_nan.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_pow_int.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_minmax.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_errno.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_roots.h:
+src_v4.5.7/comm/ODE.h:
+/home/groups/ping_group/software/libs/gsl-2.7.1/build/include/gsl/gsl_odeiv2.h:
+src_v4.5.7/comm/matrix3.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/comm/Units.h:
+src_v4.5.7/comm/myio.h:
+src_v4.5.7/comm/constants.h:
+src_v4.5.7/comm/myarray.h:
+src_v4.5.7/comm/Random.h:
+src_v4.5.7/comm/mymatrix.h:
+src_v4.5.7/comm/sparse_matrix.h:
+src_v4.5.7/comm/sparse2D.h:
+src_v4.5.7/comm/Histogram.h:
+src_v4.5.7/comm/GaussianSmapling.h:
+src_v4.5.7/comm/kmap.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/parameters.h:
+src_v4.5.7/electron.h:
+src_v4.5.7/PumpProbe.h:
+src_v4.5.7/Scatt_Param.h:
